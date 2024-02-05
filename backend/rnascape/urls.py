82016eedd7c730cf1ee10851f0dd4e02039ccde6
ariscohen <33535386+ariscohen@users.noreply.github.com>
2024-02-05 15:24:45 -0800

    Delete backend directory

--- backend/rnascape/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('run-rnascape/', views.run_rnascape, name='run_rnascape'),
-    path('run-regen_plot/', views.run_regen_plot, name='run_regen_plot'),
-    path('get-npz-file/', views.get_npz_file, name='get_npz_file'),
-    path('get-log-file/', views.get_log_file, name='get_log_file'),
-]
